0a5b83c9e92035ec00498538bd921944b780d3b8
jayati2009 <129654181+jayati2009@users.noreply.github.com>
2023-04-02 14:17:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+noseX=0;
+noseY=0;
+rightWristX=0;
+leftWristX=0;
+difference= 0;
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas= createCanvas(550, 500);
+    canvas.position(560, 150);
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}    
